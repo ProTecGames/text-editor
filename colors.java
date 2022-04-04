497d5a38294a4005a4b9be4673b956be7f5ff669
Prakhar Doneria <gamesprotec@gmail.com>
2022-04-04 10:35:57 +0530

    Create colors.java

---
+++ colors.java
@@ -0,0 +1,7 @@
+<resources>
+	<color name="colorPrimary">#FFC107</color>
+	<color name="colorPrimaryDark">#FF6F00</color>
+	<color name="colorAccent">#2196F3</color>
+	<color name="colorControlHighlight">#FFE082</color>
+	<color name="colorControlNormal">#FFD54F</color>
+</resources>
